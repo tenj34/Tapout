fcd8a9bddf67f9ef8ac966d4c600d5e85804d578
Juan Tenezaca <tenj34@gmail.com>
2018-11-25 19:08:19 +0000

    add /days/new in days controller with no view

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,2 +1,11 @@
 class DaysController < ApplicationController
+
+  get "/days/new" do
+    if logged_in?
+      @user = current_user
+      erb :'days/new'
+    else
+      redirect to '/login'
+    end
+  end
 end
